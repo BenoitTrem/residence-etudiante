fd0a456d34daffa714374187165b4f50920954e9
felix <6244794@cegepoutaouais.qc.ca>
2026-03-12 14:28:51 -0400

    Ajout de l'interface et du Repository pour Demande

---
+++ S14_ProjetSession/Data/DbDemandeRepository.cs
@@ -0,0 +1,28 @@
+﻿using S14_ProjetSession.Models;
+
+namespace S14_ProjetSession.Data
+{
+    public class DbDemandeRepository : IDemandeRepository
+    {
+        private ResidencesDbContext _context;
+        public List<Demande> Demandes => _context.Demandes.ToList();
+
+        public DbDemandeRepository(ResidencesDbContext context) 
+        {
+        }
+        public void Creer(Demande demande)
+        {
+            _context.Demandes.Add(demande);
+        }
+
+        public Demande? GetDemande(int id)
+        {
+            return _context.Demandes.FirstOrDefault(d => d.Id == id);
+        }
+
+        public void Supprimer(Demande demande)
+        {
+            _context.Demandes.Remove(demande);
+        }
+    }
+}
---
+++ S14_ProjetSession/Data/IDemandeRepository.cs
@@ -0,0 +1,16 @@
+﻿using S14_ProjetSession.Models;
+
+namespace S14_ProjetSession.Data
+{
+    public interface IDemandeRepository
+    {
+        
+        public List<Demande> Demandes { get; }
+        
+        public Demande? GetDemande(int id);
+        
+        public void Creer(Demande demande);
+
+        public void Supprimer(Demande demande);
+    }
+}
