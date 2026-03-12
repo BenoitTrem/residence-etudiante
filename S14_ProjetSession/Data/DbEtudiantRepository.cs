25a2f579b91bd040f16a8eba35eba85d764dfb42
JohnnyZFS <2257006@cegepoutaouais.qc.ca>
2026-03-12 14:20:35 -0400

    Interface et repo Etudiants

---
+++ S14_ProjetSession/Data/DbEtudiantRepository.cs
@@ -0,0 +1,43 @@
+﻿using S14_ProjetSession.Models;
+using System;
+
+namespace S14_ProjetSession.Data
+{
+    public class DbEtudiantRepository : IEtudiantRepository
+    {
+        private ResidencesDbContext _context;
+
+        public List<Etudiant> Etudiants => _context.Etudiants.ToList();
+
+
+
+        public DbEtudiantRepository(ResidencesDbContext contexte)
+        {
+            _context = contexte;
+        }
+
+ 
+        public Etudiant? GetEtudiant(int id)
+        {
+            return Etudiants.FirstOrDefault(f => f.Id == id);
+        }
+
+        public void Creer(Etudiant etudiant)
+        {
+            _context.Etudiants.Add(etudiant);
+            _context.SaveChanges();
+        }
+
+        public void Modifier(Etudiant etudiant)
+        {
+            _context.Etudiants.Update(etudiant);
+            _context.SaveChanges();
+        }
+
+        public void Supprimer(Etudiant etudiant)
+        {
+            _context.Etudiants.Remove(etudiant);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ S14_ProjetSession/Data/IEtudiantRepository.cs
@@ -0,0 +1,28 @@
+﻿using S14_ProjetSession.Models;
+
+namespace S14_ProjetSession.Data
+{
+    public interface IEtudiantRepository
+    {
+
+  
+        public List<Etudiant> Etudiants { get; }
+
+        public Etudiant? GetEtudiant(int id);
+
+    
+        public List<Etudiant> GetEtudiants(int etudiantId);
+
+
+        public void SupprimerParEtudiant(int etudiantId);
+
+      
+        public void Creer(Etudiant etudiant);
+
+       
+        public void Modifier(Etudiant etudiant);
+
+   
+        public void Supprimer(Etudiant etudiant);
+    }
+}
