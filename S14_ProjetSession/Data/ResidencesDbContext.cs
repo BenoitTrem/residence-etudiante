45ed4cb2eb5935fc5a7106a9ed41a97b69f10369
felix <6244794@cegepoutaouais.qc.ca>
2026-03-12 14:11:54 -0400

    ajout du dbContexte

--- S14_ProjetSession/Data/ResidencesDbContext.cs
+++ S14_ProjetSession/Data/ResidencesDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace S14_ProjetSession.Data
-{
-    public class ResidencesDbContext
+﻿using Microsoft.EntityFrameworkCore;
+using S14_ProjetSession.Models;
+
+namespace S14_ProjetSession.Data;
+    public class ResidencesDbContext : DbContext
     {
+        public ResidencesDbContext(DbContextOptions<ResidencesDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Demande> Demandes { get; set; }
+        public DbSet<Etudiant> Etudiants { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<PrefDureeBails> PrefDurees { get; set; }
+        public DbSet<Programme> Programmes { get; set; }
+
     }
-}
+
