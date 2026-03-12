7af8f56e577c7cd87307c741c9dcb276dbb26c8b
felix <6244794@cegepoutaouais.qc.ca>
2026-03-12 14:55:36 -0400

    Ajout de l'entité sessions

---
+++ S14_ProjetSession/Models/session.cs
@@ -0,0 +1,9 @@
+﻿namespace S14_ProjetSession.Models
+{
+    public class session
+    {
+        public int Id { get; set; }
+        // nom de la sessions exemple hivers-2025
+        public String nom {  get; set; }
+    }
+}
