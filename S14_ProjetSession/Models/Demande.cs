1c99528bb7375b40d87f85916c66b41b6958642e
felix <6244794@cegepoutaouais.qc.ca>
2026-03-12 13:54:44 -0400

    ajout verification pour le email dans demande

--- S14_ProjetSession/Models/Demande.cs
+++ S14_ProjetSession/Models/Demande.cs
@@ -1,4 +1,6 @@
-﻿namespace S14_ProjetSession.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace S14_ProjetSession.Models
 {
     public class Demande
     {
@@ -7,12 +9,18 @@
 
         public int session {  get; set; }
         // devrait etre Genre
+        []
         public String preferencesGenre  { get; set; }
         public bool JumelageVolontaire { get; set; }
 
         public String nomJumelage {  get; set; }
 
         // faire verif de email avec regex
+
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [Display(Name = "Email Address")]
         public String courrielJumelage { get; set; }
 
         public int prefDureeBail { get; set; }
