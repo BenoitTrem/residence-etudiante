f9118658e570de454c9e99c6aebfeff0753a5d52
felix <6244794@cegepoutaouais.qc.ca>
2026-03-16 13:01:52 -0400

    refactor pour Session en bon case

---
+++ S14_ProjetSession/Models/Session.cs
@@ -0,0 +1,9 @@
+﻿namespace S14_ProjetSession.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        // nom de la sessions exemple hivers-2025
+        public String Nom {  get; set; }
+    }
+}
