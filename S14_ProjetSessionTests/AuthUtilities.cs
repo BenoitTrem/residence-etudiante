bd4e2a2959274938a536c2009843227ed25135f5
Ben <2027678@cegepoutaouais.qc.ca>
2026-04-04 18:11:07 -0400

    Correction bug dans AuthUtilities

--- S14_ProjetSessionTests/AuthUtilities.cs
+++ S14_ProjetSessionTests/AuthUtilities.cs
@@ -4,6 +4,18 @@ namespace S14_ProjetSessionTests
 {
     public static class AuthUtilities
     {
+
+        public static ClaimsPrincipal CreerEtudiant()
+        {
+            return new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "468a4852-42ee-4f45-9be5-41422b589904"),
+                new Claim(ClaimTypes.Name, "amin admin"),
+                new Claim(ClaimTypes.Email, "amin.admin@exemple.com"),
+                new Claim(ClaimTypes.Role, "Utilisateur")
+            }, "TestAuth"
+            ));
+        }
         public static ClaimsPrincipal CreerAdmin()
         {
             return new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
