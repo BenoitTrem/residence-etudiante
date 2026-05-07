37db450be2fafcf9cc4e1281529df47b3792b3b3
Ben <2027678@cegepoutaouais.qc.ca>
2026-05-07 13:23:15 -0400

    Correction d'un bug avec la suppression d'une résidence ou unité lorsqu'un étudiant était associé et modification de mon audit.

---
+++ S14_ProjetSession/Migrations/20260507170138_correctionDeleteCascadeDemande.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S14_ProjetSession.Migrations
+{
+    /// <inheritdoc />
+    public partial class correctionDeleteCascadeDemande : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Demandes_Unites_UniteId",
+                table: "Demandes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Demandes_Unites_UniteId",
+                table: "Demandes",
+                column: "UniteId",
+                principalTable: "Unites",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Demandes_Unites_UniteId",
+                table: "Demandes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Demandes_Unites_UniteId",
+                table: "Demandes",
+                column: "UniteId",
+                principalTable: "Unites",
+                principalColumn: "Id");
+        }
+    }
+}
